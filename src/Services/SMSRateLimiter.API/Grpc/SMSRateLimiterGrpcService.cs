c99199b8c6b333adf228a5b43e86191a872a989c
LAPTOP-MA3GP7I5\alwie <alwien.dippenaar@hotmail.com>
2025-02-12 18:46:48 -0800

    Add gRPC functionality

---
+++ src/Services/SMSRateLimiter.API/Grpc/SMSRateLimiterGrpcService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using SMSRateLimiterAPI;
+
+namespace SMSRateLimiter.API.Grpc;
+
+public class SMSRateLimiterGrpcService(SMSRateLimiterServices services) : SMSRateLimiterGrpc.SMSRateLimiterGrpcBase {
+
+    /// <summary>
+    ///     A gRPC method to determine if a SMS message can be sent for a given phone number and account
+    ///     Better performance over the HTTP implementation.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public override async Task<CanSendResponse> CanSendSMSMessage(CanSendSMSRequest request, ServerCallContext context) {
+        try {
+            if (request == null || string.IsNullOrEmpty(request.PhoneNumber)) {
+                return new CanSendResponse() { CanSend = false, Reason = "Invalid Request" };
+            }
+
+            var canSendResponse = await services.CanSendSMSMessageAsync(request.PhoneNumber, request.AccountId);
+
+            return new CanSendResponse() { CanSend = canSendResponse.Item1, Reason = canSendResponse.Item2 };
+        } catch (Exception ex) {
+            services.Logger.LogError(ex, $"An error occured: {ex.Message}");
+            return new CanSendResponse() {
+                CanSend = false,
+                Reason = ex.Message
+            };
+        }
+    }
+}
